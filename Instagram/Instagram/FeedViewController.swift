59081eaca6df66e9fe86fd0cc409983c3780f833
Cem Bıçakcı <cmbicakci@gmail.com>
2023-07-16 21:46:25 +0300

    feed cell

--- Instagram/Instagram/FeedViewController.swift
+++ Instagram/Instagram/FeedViewController.swift
@@ -7,23 +7,34 @@
 
 import UIKit
 
-class FeedViewController: UIViewController {
-
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //Prorotype cell i 1 yaptıktan sonra, tableViewCell ismine "cell" dedik.
+        //Burada oluşturduğumuz cell isimle tableViewCell e ulaşıyoruz içerisini dolduruyoruz.
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! FeedCell
+        cell.userEmail.text = "test.gmail.com"
+        cell.commentLabel.text = "comment"
+        cell.likeLabel.text = "1"
+        cell.userImageView.image = UIImage(named: "click")
+        return cell
+    }
 
 }
