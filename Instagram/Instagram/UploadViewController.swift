ed5bf821cbf65bd16f0861247e5a1dd192032ce3
Cem Bıçakcı <cmbicakci@gmail.com>
2023-07-16 17:50:41 +0300

    upload file to firebase

--- Instagram/Instagram/UploadViewController.swift
+++ Instagram/Instagram/UploadViewController.swift
@@ -6,24 +6,69 @@
 //
 
 import UIKit
+import Firebase
 
-class UploadViewController: UIViewController {
-
+class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var commentText: UITextField!
+    @IBOutlet weak var uploadButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
+        imageView.addGestureRecognizer(gestureRecognizer)
 
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func chooseImage() {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true)
     }
-    */
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true)
+    }
+    
+    @IBAction func uploadButtonClicked(_ sender: Any) {
+        
+        let storage = Storage.storage()
+        let storageRef = storage.reference()
+    
+        //storage da oluşturduğumuz media klasörüne ulaşıyoruz.
+        let mediaFolder = storageRef.child("media")
+        
+        //image i data ya çeviriyoruz.
+        if let data = imageView.image?.jpegData(compressionQuality: 0.5){
+            
+            let imageReference = mediaFolder.child("image.jpg")
+            imageReference.putData(data, metadata: nil) { metadata, error in
+                
+                if(error != nil){
+                    print("1111")
+                    print(error?.localizedDescription)
+                }else{
+                    imageReference.downloadURL { url, error in
+                        
+                        if(error == nil){
+                            print("2222")
+                            let imageUrl = url?.absoluteString
+                            print(imageUrl)
+                        }
+                        
+                    }
+                }
+                
+            }
+            
+        }
+        
+    }
+    
 }
