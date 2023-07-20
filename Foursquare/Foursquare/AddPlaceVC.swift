006f55cb889820d973427bb18dfb1498b8059eba
Cem Bıçakcı <cmbicakci@gmail.com>
2023-07-20 20:25:33 +0300

    places vc added

---
+++ Foursquare/Foursquare/AddPlaceVC.swift
@@ -0,0 +1,27 @@
+//
+//  AddPlaceVC.swift
+//  Foursquare
+//
+//  Created by Cem Bıçakcı on 20.07.2023.
+//
+
+import UIKit
+
+class AddPlaceVC: UIViewController {
+    
+    
+    @IBOutlet weak var placNameText: UITextField!
+    @IBOutlet weak var placeTypeText: UITextField!
+    @IBOutlet weak var atmosphereText: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func nextButtonClicked(_ sender: Any) {
+    }
+    
+}
