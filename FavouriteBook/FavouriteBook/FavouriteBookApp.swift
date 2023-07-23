2619608c881b9556a0d200ee3d64769c3f6e0eab
Cem Bıçakcı <cmbicakci@gmail.com>
2023-07-23 14:29:51 +0300

    favourite book

---
+++ FavouriteBook/FavouriteBook/FavouriteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteBookApp.swift
+//  FavouriteBook
+//
+//  Created by Cem Bıçakcı on 23.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct FavouriteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
