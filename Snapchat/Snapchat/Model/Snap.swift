72570c9ee20bc919da2e73898ae66b93ed2f415f
Cem Bıçakcı <cmbicakci@gmail.com>
2023-07-27 19:30:00 +0300

    fetch data

---
+++ Snapchat/Snapchat/Model/Snap.swift
@@ -0,0 +1,14 @@
+//
+//  Snap.swift
+//  Snapchat
+//
+//  Created by Cem Bıçakcı on 27.07.2023.
+//
+
+import Foundation
+
+struct Snap {
+    var username: String
+    var imageUrlArray: [String]
+    var date: Date
+}
--- Snapchat/Snapchat/ViewControllers/FeedVC.swift
+++ Snapchat/Snapchat/ViewControllers/FeedVC.swift
@@ -8,16 +8,62 @@
 import UIKit
 import Firebase
 
-class FeedVC: UIViewController {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
     let fireStoreDatabase = Firestore.firestore()
+    var snapArray = [Snap]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
 
         getUserInfo()
+        getSnapsFromFirebase()
+    }
+    
+    func getSnapsFromFirebase(){
+        fireStoreDatabase.collection("Snaps").order(by: "date", descending: true).addSnapshotListener { snapshot, error in
+            if(error != nil){
+                self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error")
+                
+            }else{
+                if snapshot?.isEmpty == false && snapshot != nil{
+                    
+                    self.snapArray.removeAll(keepingCapacity: false)
+                    
+                    for document in snapshot!.documents{
+                        
+                        let documentId = document.documentID
+                        
+                        if let username = document.get("snapOwner") as? String {
+                            if let imageUrlArray = document.get("imageUrlArray") as? [String] {
+                                if let date = document.get("date") as? Timestamp {
+                                    
+                                    if let difference = Calendar.current.dateComponents([.hour], from: date.dateValue(), to: Date()).hour {
+                                        if difference >= 24 {
+                                            //Delete
+                                            self.fireStoreDatabase.collection("Snaps").document(documentId).delete { error in
+                                                    
+                                            }
+                                        }
+                                        
+                                        //Time Left -> SnapVC
+                                    }
+                                    
+                                    let snap = Snap(username: username, imageUrlArray: imageUrlArray, date: date.dateValue())
+                                    self.snapArray.append(snap)
+                                }
+                            }
+                        }
+                        
+                    }
+                }
+            }
+        }
     }
     
     func getUserInfo(){
@@ -44,6 +90,16 @@ class FeedVC: UIViewController {
         let okButton = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default)
         alert.addAction(okButton)
         self.present(alert, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return snapArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! FeedCell
+        cell.feedUserNameLabel.text = snapArray[indexPath.row].username
+        return cell
     }
 
 }
