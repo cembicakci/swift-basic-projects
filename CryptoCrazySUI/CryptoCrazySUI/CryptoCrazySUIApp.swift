3a456298a96f2d74b182a69b995db9d366411a53
Cem Bıçakcı <cmbicakci@gmail.com>
2023-07-29 18:34:27 +0300

    crypto-crazy

---
+++ CryptoCrazySUI/CryptoCrazySUI/CryptoCrazySUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCrazySUIApp.swift
+//  CryptoCrazySUI
+//
+//  Created by Cem Bıçakcı on 29.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCrazySUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
